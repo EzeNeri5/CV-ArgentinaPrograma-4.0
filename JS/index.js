7ae28a63f429de25b9f78efe6265e5274fa5334d
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-12-31 10:48:21 -0300

    Primera carga

---
+++ JS/index.js
@@ -0,0 +1,61 @@
+document.getElementById('azul').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#00f';
+})
+document.getElementById('rojo').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#f00';
+})
+document.getElementById('celeste').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#0bf';
+})
+document.getElementById('verde').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#0f0';
+})
+document.getElementById('anaranjado').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#f48c06';
+})
+document.getElementById('violeta').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#7400b8';
+})
+document.getElementById('marron').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#6f4518';
+})
+document.getElementById('natural').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#f7fff7';
+})
+
+document.getElementById('oculto').addEventListener("click", function(){
+    document.getElementById('demo').style.display = 'none';
+})
+document.getElementById('mostrar').addEventListener("click", function(){
+    document.getElementById('demo').style.display = 'block';
+})
+document.getElementById('oculto2').addEventListener("click", function(){
+    document.getElementById('demo2').style.display = 'none';
+})
+document.getElementById('mostrar2').addEventListener("click", function(){
+    document.getElementById('demo2').style.display = 'block';
+})
+document.getElementById('oculto3').addEventListener("click", function(){
+    document.getElementById('demo3').style.display = 'none';
+})
+document.getElementById('mostrar3').addEventListener("click", function(){
+    document.getElementById('demo3').style.display = 'block';
+})
+document.getElementById('oculto4').addEventListener("click", function(){
+    document.getElementById('demo4').style.display = 'none';
+})
+document.getElementById('mostrar4').addEventListener("click", function(){
+    document.getElementById('demo4').style.display = 'block';
+})
+document.getElementById('oculto5').addEventListener("click", function(){
+    document.getElementById('demo5').style.display = 'none';
+})
+document.getElementById('mostrar5').addEventListener("click", function(){
+    document.getElementById('demo5').style.display = 'block';
+})
+document.getElementById('oculto6').addEventListener("click", function(){
+    document.getElementById('demo6').style.display = 'none';
+})
+document.getElementById('mostrar6').addEventListener("click", function(){
+    document.getElementById('demo6').style.display = 'block';
+})
